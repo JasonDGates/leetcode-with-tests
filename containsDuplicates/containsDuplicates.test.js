f88411d8747c1aebc76580af944f3602b8ba490f
Jason Gates <gatesjasond@gmail.com>
2022-10-09 10:33:29 -0500

    segregated problems

--- containsDuplicates/containsDuplicates.test.js
+++ containsDuplicates/containsDuplicates.test.js
@@ -3,3 +3,7 @@ const containsDuplicates = require('./containsDuplicates');
 test('[1,2,3,1] should be true', () => {
   expect(containsDuplicates([1,2,3,1])).toBe(true);
 });
+
+test('[1,2,3,4] should be false', () => {
+  expect(containsDuplicates([1,2,3,4])).toBe(false);
+});
