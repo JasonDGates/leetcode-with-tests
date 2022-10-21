8b02ce2e92a1032c6b9b27819972222b1dc71659
Jason Gates <gatesjasond@gmail.com>
2022-10-20 22:16:54 -0500

    twoSum

---
+++ twoSum/twoSum.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+ var twoSum = function (nums, target) {
+  let map = {};
+  for (let i = 0; i < nums.length; i++) {
+      if (target - nums[i] in map) {
+          return [map[target - nums[i]], i];
+      } else {
+          map[nums[i]] = i;
+      }
+  }
+  // Time Complexity O(n)
+  // Space Complexity O(n)
+  // n = nums.length
+};
